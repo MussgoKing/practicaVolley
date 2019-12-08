ed29602fa92dd303adde0dcd30a86f6f319d938a
MussgoKing <setmartinezjimenez@gmail.com>
2019-12-07 22:48:29 -0600

    práctica volley

---
+++ app/src/main/java/martinez/jimenez/volley/MainActivity.kt
@@ -0,0 +1,32 @@
+package martinez.jimenez.volley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextViewiew>(R.id.text)
+        // ...
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.google.com"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+    }
+}
